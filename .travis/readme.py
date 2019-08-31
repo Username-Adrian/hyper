3947f3f38d7fd961a5a2b43c0d33408afa4ec41b
Username-Adrian <47768706+Username-Adrian@users.noreply.github.com>
2019-08-31 09:31:31 +0800

    Update readme.py

--- .travis/readme.py
+++ .travis/readme.py
@@ -8,3 +8,4 @@ raw = f.read()
 for (i, code) in enumerate(re.findall(r'```rust([^`]*)```', raw, re.M)):
     with open('examples/readme_%s.rs' % i, 'w') as f:
         f.write('#![deny(warnings)]%s' % code)
+print('edit')        
